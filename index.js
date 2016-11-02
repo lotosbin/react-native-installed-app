38063a6c8d4e7f4c572aba865286917826e2c397
liubinbin <lotosbin@gmail.com>
2016-11-02 17:09:34 +0800

    Finish ios feature

--- index.js
+++ index.js
@@ -3,33 +3,5 @@ import {
     DeviceEventEmitter
 } from 'react-native';
 
-const RongCloudIMLib = NativeModules.RongCloudIMLibModule;
-
-var _onRongCloudMessageReceived = function(resp) {
-
-}
-DeviceEventEmitter.addListener('onRongCloudMessageReceived', resp => {
-    typeof _onRongCloudMessageReceived === 'function' && _onRongCloudMessageReceived(resp);
-});
-
-const ConversationType = {
-    PRIVATE: 'PRIVATE',
-    DISCUSSION: 'DISCUSSION',
-    SYSTEM: 'SYSTEM'
-};
-
-export default {
-    ConversationType: ConversationType,
-    onReceived (callback) {
-        _onRongCloudMessageReceived = callback;
-    },
-    initWithAppKey (appKey) {
-        return RongCloudIMLib.initWithAppKey(appKey);
-    },
-    connectWithToken (token) {
-        return RongCloudIMLib.connectWithToken(token);
-    },
-    sendTextMessage (conversationType, targetId, content) {
-        return RongCloudIMLib.sendTextMessage(conversationType, targetId, content, content);
-    }
-};
+var InstalledApp = NativeModules.InstalledApp;
+export default  InstalledApp;
--- ios/InstalledAppLib/InstalledAppLib.m
+++ ios/InstalledAppLib/InstalledAppLib.m
@@ -11,7 +11,13 @@
 @implementation InstalledAppLib
 @synthesize bridge = _bridge;
 
-RCT_EXPORT_MODULE(InstalledAppModule)
+RCT_EXPORT_MODULE(InstalledApp);
 
-
+RCT_EXPORT_METHOD(isApplicationInstalled:(NSString *)name
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    BOOL b = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:name]];
+    resolve([NSNumber numberWithBool:b]);
+}
 @end
